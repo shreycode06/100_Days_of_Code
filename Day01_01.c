3e23e25e10ff3d2c496b242fa6ab9b80a1c8bd60
Shreshtha Awasthi <shreshtha.awasthi229@gmail.com>
2025-11-17 01:49:34 +0530

    1st one

---
+++ Day01_01.c
@@ -0,0 +1,29 @@
+/* 
+Q3. Write a program to input two numbers and display their sum.
+
+Sample Run:
+---------------------------------
+Enter first number: 7
+Enter second number: 12
+
+-> Sum = 19.00
+
+---------------------------------
+*/
+
+#include <stdio.h>
+
+int main() {
+    float a, b;
+
+    printf("Enter first number: ");
+    scanf("%f", &a);
+
+    printf("Enter second number: ");
+    scanf("%f", &b);
+
+    printf("\n-> Sum = %.2f\n", a + b);
+
+    return 0;
+}
+
